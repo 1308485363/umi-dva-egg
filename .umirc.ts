53376736839264168713e071231c8fb7e0673e06
1308485363@qq.com <1308485363@qq.com>
2021-06-04 16:31:20 +0800

    fix: request edit

--- .umirc.ts
+++ .umirc.ts
@@ -14,10 +14,10 @@ export default defineConfig({
   dva: {
     
   },
-  proxy: {
-    '/api': {
-      'target': 'http://127.0.0.1:10241/',
-      'changeOrigin': true,
-    },
-  },
+  // proxy: {
+  //   '/api': {
+  //     'target': 'http://127.0.0.1:10241/',
+  //     'changeOrigin': true,
+  //   },
+  // },
 });
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -6,29 +6,36 @@ axios.interceptors.response.use((response) => {
     return Promise.reject(err)
 })
 
-export const $get = (url: string, params: object) => {
-    console.log(url, params)
-    axios.get(url, params)
-    .then((response) => {
-        console.log(response)
-        return response;
+const generateRequest = (method: any, url: string, params: object) => {
+    return axios({
+        method,
+        url,
+        params,
     })
-    .catch(function (error) {
+    .then((response) => {
+        return response.data;
+    })
+    .catch(error => {
         if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
-          console.log(error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log('Error', error.message);
-        }
-        console.log(error.config);
-    });
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+          }
+          console.log(error.config);
+    })
 }
+
+export const $get = (url: string, params: object) => generateRequest('get', url, params);
+export const $post = (url: string, params: object) => generateRequest('post', url, params);
+export const $put = (url: string, params: object) => generateRequest('put', url, params);
+export const $delete = (url: string, params: object) => generateRequest('delete', url, params);
