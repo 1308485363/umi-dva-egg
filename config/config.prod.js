ea6dc4978df2096bb056f64426bb88c77246b206
1308485363@qq.com <1308485363@qq.com>
2021-06-23 19:59:22 +0800

    fix: prod set

--- config/config.prod.js
+++ config/config.prod.js
@@ -12,5 +12,11 @@ module.exports = (appInfo) => {
     gzip: true,
   };
 
+  config.listen = {
+    port: 10341,
+    hostname: '127.0.0.1', // 不建议设置 hostname 为 '0.0.0.0'，它将允许来自外部网络和来源的连接，请在知晓风险的情况下使用
+    // path: '/var/run/egg.sock',
+  };
+
   return config;
 };
