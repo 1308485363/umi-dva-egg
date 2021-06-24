93e5818448e8e7f816058f6edc2cbb2894512add
1308485363@qq.com <1308485363@qq.com>
2021-06-24 10:48:02 +0800

    fix: prod ip 127

--- config/config.prod.js
+++ config/config.prod.js
@@ -14,7 +14,7 @@ module.exports = (appInfo) => {
 
   config.listen = {
     port: 10341,
-    hostname: '0.0.0.0', // 不建议设置 hostname 为 '0.0.0.0'，它将允许来自外部网络和来源的连接，请在知晓风险的情况下使用
+    hostname: '127.0.0.1', // 不建议设置 hostname 为 '0.0.0.0'，它将允许来自外部网络和来源的连接，请在知晓风险的情况下使用
     // path: '/var/run/egg.sock',
   };
 
