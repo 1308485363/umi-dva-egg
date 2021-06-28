0672b67589ffe81804bf83a40656dfe76c0ca6d2
1308485363@qq.com <1308485363@qq.com>
2021-06-28 14:19:42 +0800

    fix: prod umi test

--- .umirc.prod.ts
+++ .umirc.prod.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 export default defineConfig({
   manifest: {
     fileName: resolve(process.cwd(), './config/manifest.json'),
-    publicPath: `http://127.0.0.1:10441/app/public/`,
+    publicPath: `http://47.92.73.227:10441`,
   },
   hash: true,
 });
